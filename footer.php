435e57e3a40dae5d6b5f9fb33738c6d0839a5b1c
H Minh <hoangminhhuong2001@gmail.com>
2023-10-25 16:20:36 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!-- ##### Footer Area Start ##### -->
+<footer class="footer-area">
+    <!-- Footer Logo -->
+    <div class="footer-logo mb-100">
+        <a href="index.php"><img src="img/core-img/logo.png" alt=""></a>
+    </div>
+    <!-- Footer Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="footer-content text-center">
+                    <!-- Social Info -->
+                    <div class="footer-social-info">
+                        <a href="#" data-toggle="tooltip" data-placement="top" title="Facebook">Facebook</a>
+                        <a href="#" data-toggle="tooltip" data-placement="top" title="Đại học Bách Khoa Hà Nội">HUST</a>
+                        <a href="#" data-toggle="tooltip" data-placement="top" title="Trường Công Nghệ thông tin và Truyền thông">SoICT</a>
+                    </div>
+
+                    <p class="mb-15">Trường Công nghệ thông tin và Truyền thông</p>
+                </div>
+            </div>
+            </sdiv>
+        </div>
+</footer>
+<!-- ##### Footer Area Start ##### -->
